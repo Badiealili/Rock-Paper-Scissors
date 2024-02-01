689cae8d4da1c7fdc59258e311d8bcdd5c3a6f0f
Badie Alili <yacineabdelbadiealili@gmail.com>
2024-02-01 13:45:37 +0100

    Add score bar functionality and dynamic update

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// Game functionality
-
+// Global Variables
 let choices = ["rock", "paper", "scissors"];
+let humanChoice;
+let computerChoice;
+let numberOfRounds = 0;
+let globalResult = 0;
+let globalHumanResult = 0;
+let globalComputerResult = 0;
 
-let getComputerChoice = function () {
+let humanScoreElement = document.querySelector(".human-score");
+let computerScoreElement = document.querySelector(".computer-score");
+
+// Helper Functions
+// Generate random computer choice
+function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
-};
+}
 
-let playRound = function (computerChoice, humanChoice) {
+// Play one round and return a numerical value representing the result
+function playRound(computerChoice, humanChoice) {
   let computerChoiceValue = choices.indexOf(computerChoice.toLowerCase());
   let humanChoiceValue = choices.indexOf(humanChoice.toLowerCase());
   let diff = humanChoiceValue - computerChoiceValue;
@@ -26,20 +37,30 @@ let playRound = function (computerChoice, humanChoice) {
   }
 
   return gameState;
-};
+}
 
-// Global Variables
-let humanChoice;
-let computerChoice;
-let numberOfRounds = 0;
-let globalResult = 0;
-let globalHumanResult = 0;
-let globalComputerResult = 0;
+// Reset the page including styles and global variables
+function resetToDefault() {
+  numberOfRounds = 0;
+  globalResult = 0;
+  globalHumanResult = 0;
+  globalComputerResult = 0;
+
+  humanScoreElement.style.flexGrow = "1";
+  computerScoreElement.style.flexGrow = "1";
+}
+
+// Update the score bar display according to the current result
+function updateScoreDisplay() {
+  humanScoreElement.style.flexGrow = `${globalHumanResult}`;
+  computerScoreElement.style.flexGrow = `${globalComputerResult}`;
+  humanScoreElement.querySelector('p').textContent = `YOU: ${globalHumanResult}`;
+  computerScoreElement.querySelector('p').textContent = `COMPUTER: ${globalComputerResult}`;
+}
 
 // Click event listener and game logic
-
-let playCards = document.querySelector(".play-cards");
-playCards.addEventListener("click", (event) => {
+let playCardsElement = document.querySelector(".play-cards");
+playCardsElement.addEventListener("click", (event) => {
   if (!event.target.classList.contains("play-card")) {
     return;
   }
@@ -47,20 +68,21 @@ playCards.addEventListener("click", (event) => {
   humanChoice = event.target.dataset.value;
   computerChoice = getComputerChoice();
   roundResult = playRound(computerChoice, humanChoice);
-  if(roundResult == 1){
-    globalHumanResult++
+  if (roundResult == 1) {
+    globalHumanResult++;
   } else {
     globalComputerResult -= roundResult;
   }
   globalResult += roundResult;
-  numberOfRounds++
+  numberOfRounds++;
   alert(
     `You chose ${humanChoice}, Computer chose ${computerChoice}, Result: ${roundResult}`
   );
   // Show the result of the game (5 rounds)
-  if(numberOfRounds == 5){
-    numberOfRounds = 0;
-    alert(`Global Result : ${globalResult}`)
+  if (numberOfRounds == 5) {
+    alert(`Global Result : ${globalResult}`);
+    resetToDefault();
   }
-});
 
+  updateScoreDisplay();
+});
